f587f25d68deab93ec6dd55ef62ff29cd489a423
defunsm <salmanhossain500@gmail.com>
2022-01-27 20:21:19 -0500

    testing accuracy

--- NYC_Airbnb/helpers.py
+++ NYC_Airbnb/helpers.py
@@ -6,5 +6,7 @@ def check_accuracy_of_model(reg, X_train, y_train):
     """Prints the accuracy of the model by computing a cross val score"""
     
     results = cross_val_score(estimator = reg, X = X_train, y = y_train, cv=10)
+    
     print('Accuracy: {:.2f} %'.format(results.mean()*100))
+    
     
