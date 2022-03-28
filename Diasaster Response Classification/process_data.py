85e5a49c0825a0c5e5c95fe084988097c8431c58
defunsm <salmanhossain500@gmail.com>
2022-03-28 15:38:34 -0400

    project components

---
+++ Diasaster Response Classification/process_data.py
@@ -0,0 +1,13 @@
+# ETL Pipeline
+
+""" 
+ETL Pipeline
+
+process_data.py, cleaning pipeline that:
+
+    Loads the messages and categories datasets
+    Merges the two datasets
+    Cleans the data
+    Stores it in a SQLite database
+    
+"""
---
+++ Diasaster Response Classification/train_classifier.py
@@ -0,0 +1,13 @@
+"""
+    ML Pipeline
+
+    train_classifier.py, machine learning pipeline that:
+
+    Loads data from the SQLite database
+    Splits the dataset into training and test sets
+    Builds a text processing and machine learning pipeline
+    Trains and tunes a model using GridSearchCV
+    Outputs results on the test set
+    Exports the final model as a pickle file
+
+"""
