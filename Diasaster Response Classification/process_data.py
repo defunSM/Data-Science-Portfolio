eb6cc3d94b3ab0f9202137eb0c7d26dc0eaa053b
defunsm <salmanhossain500@gmail.com>
2022-03-29 15:55:59 -0400

    arguments for process_data pipeline

--- Diasaster Response Classification/process_data.py
+++ Diasaster Response Classification/process_data.py
@@ -10,4 +10,42 @@ process_data.py, cleaning pipeline that:
     Cleans the data
     Stores it in a SQLite database
     
+    Example of Usage:
+    python process_data.py disaster_messages.csv disaster_categories.csv DisasterResponse.db
+    
 """
+
+# Importing libraries for ETL
+import numpy as np
+import pandas as pd
+from sqlalchemy import create_engine
+import sys
+
+def main():
+
+    # Checks if the ETL pipeline is given proper arguments and throws error otherwise
+    # TODO: Improve this later to identify which arguments are missing
+    try:
+        sys.argv[1]
+        sys.argv[2]
+        sys.argv[3]
+    except IndexError:
+        print("Usage Error: This script takes 3 arguments, the messages csv, categories csv and the name of the database.")
+        print("Example Usage: python process_data.py disaster_messages.csv disaster_categories.csv DisasterResponse.db")
+        return -1
+    
+    # Reading from the disaster messages
+    try:
+        messages = pd.read_csv(sys.argv[1])
+    except:
+        print("Error: Failed to read the first csv argument")
+    
+    # Reading from the disaster categories
+    try:
+        categories = pd.read_csv(sys.argv[2])
+    except:
+        print("Error: Failed to read the second csv argument")
+    
+if __name__ == '__main__':
+    main()
+    
