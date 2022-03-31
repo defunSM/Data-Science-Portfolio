29ba371f9c15d29813d06649bf29c3ef2b3fb9dc
defunsm <salmanhossain500@gmail.com>
2022-03-31 13:14:32 -0400

    create load_data, tokenize, and imports

--- Diasaster Response Classification/train_classifier.py
+++ Diasaster Response Classification/train_classifier.py
@@ -13,9 +13,63 @@
     Example Usage: python train_classifier.py ../data/DisasterResponse.db classifier.pkl
 
 """
+# import libraries
+import numpy as np
+import pandas as pd
+import sys
+from sqlalchemy import create_engine
+import nltk
+import joblib
 
-def load_data():
-    pass
+nltk.download('punkt')
+nltk.download('stopwords')
+
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem.porter import PorterStemmer
+
+import re
+
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer, TfidfTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.ensemble import RandomForestClassifier
+
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
+
+from sklearn.neighbors import KNeighborsClassifier
+
+
+
+def load_data(filepath):
+    
+    # Reading from the database
+    engine = create_engine('sqlite:///' + filepath)
+    df = pd.read_sql_table(filepath)
+    
+    # Setting up the X and Y dataframes
+    X = df.iloc[:,1].dropna()
+    Y = df.iloc[:,4:]
+
+def tokenize(text):
+    
+    clean_tokens = []
+    
+    # lowercasing and removing punctuation
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower().strip())
+    tokens = word_tokenize(text)
+    # Remove stop words + stem
+    stemmer = PorterStemmer()
+    
+    
+    stop_words = stopwords.words("english")
+    clean_tokens = [ stemmer.stem(word) for word in tokens if word not in stop_words]
+
+    return clean_tokens
 
 def model_data():
     pass
