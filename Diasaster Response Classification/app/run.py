825bfd138682f3886ac3e13e90e4bc0e5d81ff33
defunsm <salmanhossain500@gmail.com>
2022-03-31 13:55:18 -0400

    error can't import joblibn from sklearn.externals

--- Diasaster Response Classification/app/run.py
+++ Diasaster Response Classification/app/run.py
@@ -8,7 +8,7 @@ from nltk.tokenize import word_tokenize
 from flask import Flask
 from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar
-from sklearn.externals import joblib
+import joblib
 from sqlalchemy import create_engine
 
 
