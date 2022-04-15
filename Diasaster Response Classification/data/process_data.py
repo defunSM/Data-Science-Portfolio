30dfbd6a3048f145871588885e1c6af7f3108fef
defunsm <salmanhossain500@gmail.com>
2022-04-14 22:59:27 -0400

    bugfix: included if_exists='replace'

--- Diasaster Response Classification/data/process_data.py
+++ Diasaster Response Classification/data/process_data.py
@@ -74,7 +74,7 @@ def save_data(df, database_filename):
     """Save and clean dataset into sqlite database"""
     
     engine = create_engine('sqlite:///' + database_filename)
-    df.to_sql(database_filename, engine, index=False)
+    df.to_sql(database_filename, engine, index=False, if_exists='replace')
 
 def main():
     if len(sys.argv) == 4:
