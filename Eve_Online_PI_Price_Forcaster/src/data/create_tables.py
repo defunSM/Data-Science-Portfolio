71b97617926ebc72fbf5a0bb94104cf4e5918737
defunsm <salmanhossain500@gmail.com>
2022-05-19 22:49:25 -0400

    add: item_names, get_item_id

--- Eve_Online_PI_Price_Forcaster/src/data/create_tables.py
+++ Eve_Online_PI_Price_Forcaster/src/data/create_tables.py
@@ -10,53 +10,31 @@ dotenv_path = find_dotenv()
 # load up the entries as environment variables
 load_dotenv(dotenv_path)
 
-database_url = os.environ.get("DATABASE_URL")
-other_variable = os.environ.get("OTHER_VARIABLE")
+DATABASE_URL = os.environ.get('DATABASE_URL')
+DATABASE_NAME = os.environ.get("DATABASE_NAME")
+DATABASE_USER = os.environ.get("DATABASE_USER")
+DATABASE_PASSWORD = os.environ.get("DATABASE_PASSWORD")
 
 def create_tables():
     """ create tables in the PostgreSQL database"""
     commands = (
         """
-        CREATE TABLE vendors (
-            vendor_id SERIAL PRIMARY KEY,
-            vendor_name VARCHAR(255) NOT NULL
-        )
-        """,
-        """ CREATE TABLE parts (
-                part_id SERIAL PRIMARY KEY,
-                part_name VARCHAR(255) NOT NULL
-                )
-        """,
-        """
-        CREATE TABLE part_drawings (
-                part_id INTEGER PRIMARY KEY,
-                file_extension VARCHAR(5) NOT NULL,
-                drawing_data BYTEA NOT NULL,
-                FOREIGN KEY (part_id)
-                REFERENCES parts (part_id)
-                ON UPDATE CASCADE ON DELETE CASCADE
-        )
-        """,
-        """
-        CREATE TABLE vendor_parts (
-                vendor_id INTEGER NOT NULL,
-                part_id INTEGER NOT NULL,
-                PRIMARY KEY (vendor_id , part_id),
-                FOREIGN KEY (vendor_id)
-                    REFERENCES vendors (vendor_id)
-                    ON UPDATE CASCADE ON DELETE CASCADE,
-                FOREIGN KEY (part_id)
-                    REFERENCES parts (part_id)
-                    ON UPDATE CASCADE ON DELETE CASCADE
+        CREATE TABLE raw_market_data (
+            id SERIAL PRIMARY KEY,
+            time TIMESTAMP,
+            data JSON
         )
         """)
-    conn = None
+    
+    conn = psycopg2.connect(host=DATABASE_URL,
+                            database=DATABASE_NAME,
+                            user=DATABASE_USER,
+                            password=DATABASE_PASSWORD)
     try:
-        # read the connection parameters
-        params = config()
+
         # connect to the PostgreSQL server
-        conn = psycopg2.connect(**params)
         cur = conn.cursor()
+        
         # create table one by one
         for command in commands:
             cur.execute(command)
