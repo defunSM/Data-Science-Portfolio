4d5f7e55de7150ee7e82afb029132546d2d98fc9
defunsm <salmanhossain500@gmail.com>
2022-05-23 22:05:13 -0400

    add: hashing for items.txt

--- Eve_Online_PI_Price_Forecaster/notebooks/PI_Price_EDA.py
+++ Eve_Online_PI_Price_Forecaster/notebooks/PI_Price_EDA.py
@@ -3,7 +3,7 @@
 
 # ## Imports
 
-# In[56]:
+# In[1]:
 
 
 import psycopg2
@@ -35,22 +35,23 @@ DATABASE_PORT = os.environ.get("DATABASE_PORT")
 """---------------------- filepath and id constants --------------------------"""
 
 # Filepath that contains the items that we are interested in forcasting
-FILENAME = 'src/data/items.txt'
+ABS_FILE_PATH_ITEMS = 'src/data/items.txt'
 
 # region ids that will be used in the function collect_data 
 REGIONS = [0, 30000142, 30000144, 60003760, 60008494, 60011866, 60004588, 60005686]
 
 
-# In[57]:
+# In[2]:
 
 
 def create_tables():
     """ create tables in the PostgreSQL database"""
     command = (
         """
-        CREATE TABLE raw_market_data (
+        CREATE TABLE market_data (
             id SERIAL PRIMARY KEY,
             time TIMESTAMP,
+            region_id INTEGER,
             data JSON
         )
         """)
@@ -81,7 +82,7 @@ def create_tables():
             conn.close()
 
 
-# In[58]:
+# In[3]:
 
 
 #create_tables()
@@ -94,11 +95,11 @@ def create_tables():
 
 # ## Data Collection
 
-# In[59]:
+# In[5]:
 
 
 def get_raw_material_names():
-    """ Return a list of raw material names from from FILENAME
+    """ Return a list of raw material names from from ABS_FILE_PATH_ITEMS
 
     Returns:
         raw_material_names: item names of interest for forcast
@@ -108,23 +109,23 @@ def get_raw_material_names():
     # List that will be returned after it has been populated
     raw_material_names = []
     
-    if path.exists(FILENAME):
+    if path.exists(ABS_FILE_PATH_ITEMS):
         
-        file = open(FILENAME, "r")
+        file = open(ABS_FILE_PATH_ITEMS, "r")
         
-        # Read each line from FILENAME and append each item name into the list
+        # Read each line from ABS_FILE_PATH_ITEMS and append each item name into the list
         for item in file.readlines():
             raw_material_names.append(item.strip('\n'))
 
         file.close()
         
+        # Make sure that the item names are not repeated
+        raw_material_names = list(set(raw_material_names))
+    
+        return raw_material_names
+        
     else:
-        print(FILENAME, "does not exist.")
-    
-    # Make sure that the item names are not repeated
-    raw_material_names = list(set(raw_material_names))
-    
-    return raw_material_names
+        print(ABS_FILE_PATH_ITEMS, "does not exist.")
 
 def get_item_id(item_name=None):
     """ Returns the item ID from using the API endpoint https://www.fuzzwork.co.uk/api/typeid.php?typename=Silicon
@@ -167,9 +168,51 @@ def fetch_data(region_id, item_id):
     
     return raw_material_data
 
+def insert_data(table_name='market_data', region_id=None, json_data=None):
+    """ Connects with postgresql database and inserts records into a table.
 
-# In[60]:
+    Args:
+        table_name (str, optional): _description_. Defaults to 'raw_material_data'.
+        region_id (int, optional): _description_. Defaults to None.
+        json_data (json, optional): _description_. Defaults to None.
+    """
+    
+    time = datetime.now()
+    
+    try:
+        conn = psycopg2.connect(host=DATABASE_URL,
+                                database=DATABASE_NAME,
+                                user=DATABASE_USER,
+                                password=DATABASE_PASSWORD)
+        cursor = conn.cursor()
+        
+        # sql to insert the json into table
+        insert_command ="INSERT INTO " + table_name + " (TIME, REGION_ID, DATA) VALUES (%s, %s, %s)"
+        
+        # Serializing json
+        json_object = json.dumps(json_data)
+        
+        # Inserting into table
+        values = (time, region_id, json_object)
+        cursor.execute(insert_command, values)
+        
+        # Close and commit changes to database server
+        conn.commit()
+        conn.close()
+        
+        print("Successfully stored data into table for region ID: " + str(region_id) + ".")
+        
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
 
+
+# In[9]:
+
+
+# TODO: Implement concurrency when doing API calls
 
 def store_data():
     """ GET Requests the Eve Online API endpoint https://market.fuzzwork.co.uk/aggregates/?region=30000142&types=9828 
@@ -193,64 +236,31 @@ def store_data():
     item_ids = [ get_item_id(i) for i in items ]
 
     json_data = {}
-    time = datetime.now()
 
     # Fetch the data for each region and raw material id
-    print("Fetching data...")
+    print("Attempting to fetch and insert data...")
     with alive_bar(len(REGIONS), force_tty=True) as bar:
         for r in REGIONS:
+            
+            # Fetching the data
             data = fetch_data(region_id=r, item_id=item_ids)
-            data['region_id'] = r
-            json_data.update(data)
+            
+            # Storing the data into a postgresql table
+            insert_data(table_name='market_data', region_id=r, json_data=data)
             bar()
     
-    # Connecting to the database
-    print("Trying to connect to database...")
-    try:
-        conn = psycopg2.connect(host=DATABASE_URL,
-                                database=DATABASE_NAME,
-                                user=DATABASE_USER,
-                                password=DATABASE_PASSWORD)
-        cursor = conn.cursor()
-        
-        print("Connected to database and now inserting data into table...")
-        
-        # sql to insert the json into the raw_market_data table
-        insert_command = (
-        """
-        INSERT INTO raw_market_data (TIME, DATA) VALUES (%s, %s)
-        """
-        )
-        
-        # Serializing json
-        json_object = json.dumps(json_data)
-        
-        # Inserting into table
-        values = (time, json_object)
-        cursor.execute(insert_command, values)
-        
-        # Close and commit changes to database server
-        conn.commit()
-        conn.close()
-        
-        print("Successfully stored data into table.")
-        
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
+    print("Successfully stored data for all regions!")
     
     
 
 
-# In[61]:
+# In[11]:
 
 
-#fetch_data(0, [34,35,36,37,38,39,40])
 
 
-# In[62]:
+
+# In[10]:
 
 
 if __name__ == "__main__":
